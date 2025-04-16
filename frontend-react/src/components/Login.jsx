9f43ee70d498dd959fefb01de8a2cda46d72425f
Siva Kumar <sivakumarjeevavantham@gmail.com>
2025-04-16 15:59:12 +0530

    authentication functionalities

--- frontend-react/src/components/Login.jsx
+++ frontend-react/src/components/Login.jsx
@@ -17,16 +17,14 @@ const Login = () => {
         e.preventDefault();
         setloading(true)
         const userData = { username, password }
-        // console.log('userData===>', userData);
+
 
         try {
             const response = await axios.post('http://127.0.0.1:8000/api/v1/token/', userData)
-            // console.log(response.data);
             localStorage.setItem('accessToken', response.data.access)
             localStorage.setItem('refreshToken', response.data.refresh)
-            // console.log('Login successful');
             setIsLoggedIn(true)
-            navigate('/')
+            navigate('/dashboard')
         } catch (error) {
             console.error('Invalid credentials');
             setError('Invalid credentials')
@@ -40,7 +38,7 @@ const Login = () => {
             <div className="container">
                 <div className="row justify-content-center">
                     <div className="col-md-6 bg-light-dark p-5 rounded">
-                        <h3 className='text-light text-center mb-4'>Create an Account</h3>
+                        <h3 className='text-light text-center mb-4'>Log to our Portal</h3>
                         <form onSubmit={handleLogin}>
                             <div className='mb-3'>
                                 <input type='text' className='form-control' placeholder='Username' value={username} onChange={(e) => setUsername(e.target.value)} />
---
+++ frontend-react/src/components/dashboard/Dashboard.jsx
@@ -0,0 +1,21 @@
+import React, { useEffect } from 'react'
+import axiosInstance from '../../axiosInstance'
+
+
+const Dashboard = () => {
+    useEffect(() => {
+        const fetchProtectedData = async () => {
+            try {
+                const response = await axiosInstance.get('/protected-view/')
+            } catch (error) {
+                console.error('Error fetching data:', error)
+            }
+        }
+        fetchProtectedData();
+    }, [])
+    return (
+        <div className='text-light container'>Dashboard</div>
+    )
+}
+
+export default Dashboard
